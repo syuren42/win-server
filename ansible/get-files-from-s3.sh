2bb93ab8d1bc6255ee81486ba072469177e6c167
syuren42 <okazaki.nsw@gmail.com>
2020-05-24 21:58:42 +0900

    Languageパックを

--- ansible/get-files-from-s3.sh
+++ ansible/get-files-from-s3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws s3 cp  s3://resource-virtual-office/win-packages/GoogleChromeStandaloneEnterprise64.msi .
+dest="/tmp"
 
+aws s3 cp  s3://resource-virtual-office/win-packages/GoogleChromeStandaloneEnterprise64.msi $dest
+aws s3 cp  s3://resource-virtual-office/win-packages/windowsserver2019-jplangpack.iso $dest
